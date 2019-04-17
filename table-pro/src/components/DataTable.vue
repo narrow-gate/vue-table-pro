e07a24e451bc0a2ab7116ff171be8104fa2a9412
hello <770955207@qq.com>
2019-04-17 23:42:04 +0800

    4/17

--- table-pro/src/components/DataTable.vue
+++ table-pro/src/components/DataTable.vue
@@ -1,70 +1,93 @@
+    
 <template>
-  <view-page>
-    <template slot="left-field">
-      <el-button type="danger" icon="el-icon-circle-plus-outline" @click="addTodo">添加</el-button>
-    </template>
-  <template slot="search-field">
-    <el-input v-model="searchStr" suffix-icon="el-icon-search" placeholder="请输入搜索内容"></el-input>
-</template>
-    <template slot="filter-field">
-      <el-select v-model="filterType" placeholder="选择类型">
-        <el-option label="全部" value></el-option>
-        <el-option v-for="(status,index) in statuses" :key="status" :value="index" :label="status"></el-option>
-      </el-select>
-      <el-date-picker type="daterange" start-placeholder="起始时间" end-placeholder="结束时间"></el-date-picker>
-    </template>
-    <template slot="right-field">
-      <el-button type="primary" icon="el-icon-refresh">刷新</el-button>
-      <el-button type="warning" icon="el-icon-upload2">导入</el-button>
-      <el-button type="success" icon="el-icon-download">导出</el-button>
-    </template>
-    <el-table :data="pagedData" @sort-change="sortChange">
-      <el-table-column type="expand">
-        <template slot-scope="scope">
-          <!--这里是table组件某一行的row进行传递-->
-          <el-card header="书籍内容简介">{{scope.row.content}}</el-card>
+    <view-page>
+        <!-- 左按钮区 -->
+        <template slot="left-field">
+            <el-button type="danger" icon="el-icon-circle-plus-outline" @click="addTodo">添加</el-button>
         </template>
-      </el-table-column><el-table-column label="学习书籍" prop="name" sortable></el-table-column>
-      <el-table-column label="作者">
-        <template slot-scope="scope">{{scope.row.author.join(',')}}</template>
-      </el-table-column>
-      <el-table-column label="学习计划状态" prop="status" sortable>
-        <template slot-scope="scope">
-          <el-tag :type="statusColors[scope.row.status]">{{statuses[scope.row.status]}}</el-tag>
+        <!-- 搜索框 -->
+        <template slot="search-field">
+            <el-input v-model="searchStr" suffix-icon="el-icon-search" placeholder="请输入搜索内容"></el-input>
         </template>
-      </el-table-column>
-      <el-table-column label="学习完成时间"prop="completeDate" sortable>
-        <template slot-scope="scope">{{new Date(scope.row.completeDate).toLocaleDateString()}}</template>
-      </el-table-column>
-      <el-table-column label="操作">
-        <template>
-          <el-button size="small" type="warning" icon="el-icon-edit"></el-button>
-          <el-button size="small" type="danger" icon="el-icon-delete"></el-button>
+        <!-- 过滤条件区 -->
+        <template slot="filter-field">
+            <!-- 状态过滤框 -->
+            <el-select v-model="filterType" placeholder="选择类型">
+                <el-option label="全部" value=""></el-option>
+                <el-option v-for="(status, index) in statuses" :key="status" :label="status" :value="index"></el-option>
+            </el-select>
+            <!-- 时间过滤框 -->
+            <el-date-picker type="daterange" start-placeholder="起始时间" end-placeholder="结束时间"></el-date-picker>
         </template>
-      </el-table-column>
-    </el-table>
-    <edit-dialog :show="editShow" title="编辑学习计划" @close="closeEditDialog" @save="saveTodo">
-      <el-form :model="currentTodo" ref="todoEditForm">
-        <el-form-item lable="学习书籍" prop="name" required>
-          <el-input v-model="currentTodo.name"></el-input>
-          </el-form-item>
-           <el-form-item lable="书籍内容" prop="content" required>
-          <el-input v-model="currentTodo.content" type="textarea"></el-input>
-          </el-form-item>
-          <el-form-item lable="完成时间" prop="completeDate" required>
-            <el-data-picker v-model="currentTodo.completeData" type="date"> </el-date-picker>
-        </el-form-item>
-        <el-form-item lable="书籍作者" prop="author" required>
-            <el-tag v-for="author in currentAuthors" :key="author"> {{author}}</el-tag>
-            <span @keyip.enter="addAuthor"><el-input v-model="currentAuthor"></el-input></span>
-            <el-button type="primary" size="small" icon="el-icon-plus" @click="addAuthor">添加作者</el-button>
-    </el-form-item>
-    </edit-dialog>
-    <el-pagination :total="total" :current-page="currentPage" 
-         :page-size="currentPageSize" :page-sizes="[1, 3]"
+        <!-- 右按钮区 -->
+        <template slot="right-field">
+            <el-button type="primary" icon="el-icon-refresh" @click="update">刷新</el-button>
+            <el-button type="warning" icon="el-icon-upload2" @click="uploadShow=true">导入</el-button>
+            <el-button type="success" icon="el-icon-download" @click="downloadTodos">导出</el-button>
+        </template>
+        <!-- 表格区 -->
+        <el-table :data="pagedData" @sort-change="sortChange">
+            <el-table-column type="expand">
+                <template slot-scope="scope">
+                    <el-card header="书籍内容简介">
+                        {{scope.row.content}}
+                    </el-card>
+                </template>
+            </el-table-column>
+            <el-table-column label="学习书籍" prop="name" sortable="custom"></el-table-column>
+            <el-table-column label="作者">
+                <template slot-scope="scope">
+                    {{scope.row.author.join(', ')}}
+                </template>
+            </el-table-column>
+            <el-table-column label="学习计划状态" prop="status" sortable="custom">
+                <template slot-scope="scope">
+                    <el-tag :type="statusColors[scope.row.status]">{{statuses[scope.row.status]}}</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column label="学习完成时间" prop="completeDate" sortable="custom">
+                <template slot-scope="scope">
+                    {{new Date(scope.row.completeDate).toLocaleDateString()}}
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" min-width="150px">
+                <template slot-scope="scope">
+                    <!-- 启动学习计划 -->
+                    <el-button v-if="scope.row.status == 0 || scope.row.status == 2" @click="updateStatusAjax(scope.row, 1)" size="small" type="primary" icon="el-icon-arrow-right"></el-button>
+                    <!-- 搁置学习计划 -->
+                    <el-button v-if="scope.row.status == 1" @click="updateStatusAjax(scope.row, 2)" size="small" type="info" icon="el-icon-loading"></el-button>
+                    <!-- 完成学习计划 -->
+                    <el-button v-if="scope.row.status == 1" @click="updateStatusAjax(scope.row, 3)" size="small" type="success" icon="el-icon-check"></el-button>
+                    <el-button size="small" type="warning" icon="el-icon-edit" @click="editTodo(scope.row)"></el-button>
+                    <el-button size="small" type="danger" icon="el-icon-delete" @click="removeTodoAjax(scope.row)"></el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination :total="total" :current-page="currentPage" 
+         :page-size="currentPageSize" :page-sizes="[3, 5]"
          layout="total, sizes, prev, pager, next, jumper"
          @size-change="pageSizeChange" @current-change="pageChange">
-</el-pagination>
+        </el-pagination>
+        <!-- 对话框 -->
+        <edit-dialog :show="editShow" title="编辑学习计划" @close="closeEditDialog" @save="saveTodo">
+            <!-- 学习内容表单 -->
+            <el-form :model="currentTodo" ref="todoEditForm">
+                <el-form-item label="学习书籍" prop="name" required>
+                    <el-input v-model="currentTodo.name"></el-input>
+                </el-form-item>
+                <el-form-item label="书籍作者" prop="author">
+                    <el-tag v-for="author in currentAuthors" :key="author" closable @close="removeAuthor(author)">{{author}}</el-tag>
+                    <span @keyup.enter="addAuthor"><el-input v-model="currentAuthor"></el-input></span>
+                    <el-button type="primary" size="small" icon="el-icon-plus" @click="addAuthor">添加作者</el-button>
+                </el-form-item>
+                <el-form-item label="书籍内容" prop="content" required>
+                    <el-input v-model="currentTodo.content" type="textarea"></el-input>
+                </el-form-item>
+                <el-form-item label="完成时间" prop="completeDate" required>
+                    <el-date-picker v-model="currentTodo.completeDate" type="date"></el-date-picker>
+                </el-form-item>
+            </el-form>
+        </edit-dialog>
   </view-page>
 </template>
 
@@ -87,7 +110,9 @@ export default {
       currentPage: 1,
         currentPageSize: 3,
   editShow:false,
-  currentTodo:{}
+  currentTodo:{},
+                currentAuthors: [],
+                currentAuthor: ''
     };
   },
   mounted() {
@@ -129,26 +154,75 @@ export default {
       this.currentTodo={}
       this.editShow=true;
     },
-    saveTodo(){
-      this.$refs.todoEditForm.vailidate((valid)=>{
-        if(valid){
-          this.currentTodo._id?this.editAjax():this.addAjax()
-        }
-      })
-      //this.closeEditDialog()
-    },
-   closeEditDialog(){
-     this.currentTodo={}
-     this.$refs.todoEditForm.resetFields()
-     this.editShow=false;
-   },
+    saveTodo() {
+                this.$refs.todoEditForm.validate((valid) => {
+                    if (valid) {
+                        this.currentTodo.author = this.currentAuthors
+                        this.currentTodo._id ? this.editAjax() : this.addAjax()
+                    }
+                });
+            },
+            closeEditDialog() {
+                this.currentTodo = {}
+                this.currentAuthors = []
+                this.currentAuthor = ''
+                this.$refs.todoEditForm.resetFields()
+                this.editShow = false
+            },
    addAjax(){
-     this.closeEditDialog()
+     this.$ajax.post('todos',this.currentTodo).then((res)=>{
+       if(res.data)
+       this.data.push(res.data)
+       this.closeEditDialog()
+     }).catch((err)=>this.$notify({
+type:'error',
+message:err
+     }))
    },
    editAjax(){
-     this.closeEditDialog()
+    this.$ajax.put('todos/'+this.currentTodo._id,this.currentTodo).then((res)=>{
+      if(res.data){
+        var index=this.data.findIndex(item=>item._id==res.data_id)
+        this.data.splice(index,1,res.data)
+      }
+      this.closeEditDialog()
+    }).catch((err)=>this.$notify({
+type:'error',
+message:err
+    }))
    },
-   addAuthor
+   addAuthor(){
+     this.currentAuthors.push(this.currentAuthor)
+     this.currentAuthor=""
+   },
+   removeAuthor(tag){
+     this.currentAuthors.splice(this.currentAuthors.indexOf(tag),1)
+   },
+   updateStatusAjax(row,status){
+     var todo={_id:row.id,status}
+     this.$ajax.put('todos/'+todo._id,todo).then((res)=>{
+       if(res.data){
+         var index=this.data.findIndex(item=>item._id==res.data._id)
+         this.data.splice(index,1,res.data)
+       }
+     }).catch((err)=>this.$notify({
+       type:'error',
+       message:err
+     }))
+   },
+   removeTodoAjax(row){
+      this.$confirm('确定要删除?').then(() => {
+        this.$ajax.delete('todos/'+row._id).then((res)=>{
+if(res.data){
+     var index = this.data.findIndex(item => item._id == res.data._id)
+                this.data.splice(index, 1)
+}
+        })
+      }).catch((err)=>this.$notify({
+ type: 'error',
+        message: err
+      }))
+   }
   },
   computed: {
     filtedData() {
